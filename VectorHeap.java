b8ded3b5c8a3798e937d7445ac57cd00fafd629c
BettiRodas <betti.rodas7@gmail.com>
2015-10-01 13:23:09 -0600

    Update VectorHeap.java

--- VectorHeap.java
+++ VectorHeap.java
@@ -1,33 +1,41 @@
+/*
+Betti Rodas 14204
+Cecilia Marsicovetere 14112
+1 de octubre de 2015
+ClaseVectorHeap
+Esta clase implementa una cola de prioridad.
+
+
+*/
 
 import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.io.*;
 
-public class VectorHeap implements PriorityQueue<Paciente> {
+public class VectorHeap/*<E extends Comparable<E>> extends PriorityQueue<Paciente>*/  {
 
 	private PriorityQueue<Paciente> colaPacientes;
 
-	public VectorHeap(object E){
-		colaPacientes = new PriorityQueue<Paciente>;
+	public VectorHeap(){
+		colaPacientes = new PriorityQueue<Paciente>();
 	}
 
+//Este metodo crea los pacientes nuevos, por medio deun parametro que es lo que se lee en la ficha del paciente. Este se encarga de separar la cadena y guardar el paciente
 	public void nuevoPaciente(String datosPaciente){
-                String[] infoPaciente = new String[3];
-		infoPaciente = datosPaciente.split(",");
+
+		String[] infoPaciente = datosPaciente.split(",");
 		Paciente paciente = new Paciente(infoPaciente[0], infoPaciente[1], infoPaciente[2]);
 		colaPacientes.add(paciente);
 	}
 
-
+//La lista completa agarra todos los elementos del heap y los concatena 
 	public String listaCompleta(){
-		String todaLaLista = null;
+		String todaLaLista="";
 		int cantidad =colaPacientes.size();
 		for (int i=0; i< cantidad; i++){
-			Paciente atendido = colaPacientes.poll();
-			todaLaLista = todaLaLista + "\n" + atendido.toString(); 
+			Paciente salida = colaPacientes.poll();
+			todaLaLista = todaLaLista + "\n" + salida.toString(); 
 		}
 		return todaLaLista;
 	}
 	
-}
-
