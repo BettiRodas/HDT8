eb5046293dfdff3f73eb9c32314d8dd2e1cc78a6
BettiRodas <betti.rodas7@gmail.com>
2015-10-01 13:27:59 -0600

    Update HojaTrabajo8.java

--- HojaTrabajo8.java
+++ HojaTrabajo8.java
@@ -1,3 +1,14 @@
+ /*
+Betti Rodas 14204
+Cecilia Marsicovetere 14112
+1 de octubre de 2015
+
+
+HOJA DE TRABAJO 8
+
+en esta se ecuentra el main. El main hace la lectura del archivo, agregando cada linea al priority heap.
+*/
+
 
 import java.io.*;
 
@@ -7,6 +18,8 @@ public class HojaTrabajo8 {
 	   	  File archivo = null;
 	      FileReader fr = null;
 	      BufferedReader br = null;
+
+	      //crea un objeto
 	      VectorHeap pacientes = new VectorHeap();
 	      
 	      try {
@@ -17,15 +30,29 @@ public class HojaTrabajo8 {
 	         // Lectura del fichero
 	         
 	         String linea;
-	         
+	         System.out.println();
+			System.out.println("Los pacientes son:");
 	         while((linea=br.readLine())!=null){
+	         	//si todavia hay datos, agarra la linea completa y la agrega a la prioridad
 	        	 pacientes.nuevoPaciente(linea);
+	        	 System.out.println(linea);
 	         }
+
+	         System.out.println();
+	         System.out.println();
+	         System.out.println();
+	         System.out.println("Los pacientes seran atendidos en este orden:");
 	 		
+
+	 		//imprime la lista de la cola ordenada
 	         String lista = pacientes.listaCompleta();
 	         System.out.println(lista);
 	      }
 
+
+
+
+	      	//en caso que falle la lectura del archivo
 	      catch(Exception e){
 	         e.printStackTrace();
 	      }finally{
@@ -40,3 +67,4 @@ public class HojaTrabajo8 {
 		
 	}
 }
+
